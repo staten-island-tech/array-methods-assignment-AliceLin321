e5e9defde0b14e3043c9b68eb5a8b96fa1a649c3
AliceLin321 <ataechiml24@gmail.com>
2022-10-28 13:55:20 -0400

    Alice Lin Array

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
 const fruit = [
     
+{
+    Fruit: [lychee]
+    Color:[red]
+    Seed:[1]
+    origin:[china, malaysia,]
+}
+{Fruit: [Mangosteen]
+    Color:[purple]
+    Seed:[1,2]
+    origin:[]
+}
+{Fruit: 
+    Color:[]
+    Seed:[]
+    origin:[]
+}
+{Fruit: 
+    Color:[]
+    Seed:[]
+    origin:[]
+}
+{Fruit: 
+    Color:[]
+    Seed:[]
+    origin:[]
+}
+{Fruit: 
+    Color:[]
+    Seed:[]
+    origin:[]
+}
+{Fruit: 
+    Color:[]
+    Seed:[]
+    origin:[]
+}
+{Fruit: 
+    Color:[]
+    Seed:[]
+    origin:[]
+}
+{Fruit: 
+    Color:[]
+    Seed:[]
+    origin:[]
+}
+{Fruit: 
+    Color:[]
+    Seed:[]
+    origin:[]
+}
+{Fruit: 
+    Color:[]
+    Seed:[]
+    origin:[]
+}
+{Fruit: 
+    Color:[]
+    Seed:[]
+    origin:[]
+}
+
 ]
+    
