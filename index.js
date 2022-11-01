aebda2835f521705f1973d8bcc1d64f3c1742e37
AliceLin321 <ataechiml24@gmail.com>
2022-11-01 13:57:55 -0400

    alice lin array

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const fruit = [
-  {
-    Fruit: [lychee],
-    Color: [red],
-    Seed: [1],
-    origin: [china, malaysia],
-  },
-  { Fruit: [mangosteen], Color: [purple], Seed: [1, 2], origin: [] },
-  { Fruit: [passionfruit], Color: [], Seed: [], origin: [] },
-  { Fruit: [papaya], Color: [], Seed: [], origin: [] },
-  { Fruit: [starfruit], Color: [], Seed: [], origin: [] },
-  { Fruit: [longan], Color: [], Seed: [], origin: [] },
-  { Fruit: [persimmon], Color: [], Seed: [], origin: [] },
-  { Fruit: [pomegranate], Color: [], Seed: [], origin: [] },
-  { Fruit: [goldenkiwi], Color: [], Seed: [], origin: [] },
+  { Fruit: [lychee], Color: [red], Seed: [1], origin: [china, malaysia], tasty: true},
+  { Fruit: [mangosteen], Color: [purple], Seed: [1,2], origin: [malaysia, indoneisa], tasty: true },
+  { Fruit: [passionfruit], Color: [purple], Seed: [60,61,62,63,64,65,66,67,68,69,70], origin:[], tasty: true },
+  { Fruit: [starfruit], Color: [yellow], Seed: [5,6,7,8,9,10], origin: [indonesia], tasty: true },
+  { Fruit: [longan], Color: [brown], Seed: [1], origin: [china, indonesia] , tasty: true},
+  { Fruit: [persimmon], Color: [orange], Seed: [5,6,7,8], origin: [china, japan, korea] , tasty: false},
+  { Fruit: [pomegranate], Color: [red], Seed: [550,560,570,580,590,600], origin: [india, iran] , tasty: true},
+  { Fruit: [kiwi], Color: [brown], Seed: [450,460,470,480,490,500], origin: [china] , tasty: true},
+  { Fruit: [kumquat], Color: [yellow], Seed: [1,2,3,4], origin: [china] , tasty: true},
+  { Fruit: [jackfruit], Color: [yellow], Seed: [100], origin: [india, malaysia] , tasty: false},
+  { Fruit: [fig], Color: [purple], Seed: [600,610,620,630,640,650], origin: [india] , tasty: false},
+ 
 ];
+Fruit.forEach((Fruit) => 
+{
+console.log(fruit.color)
+});
