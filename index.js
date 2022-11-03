89b87ad67992abf848eddd88d1f985257ce475c1
AliceLin321 <ataechiml24@gmail.com>
2022-11-03 13:58:03 -0400

    alvyiwsclbj
    abrasertb

--- index.js
+++ index.js
@@ -1,83 +1,87 @@
 const fruit = [
   {
-    name: [lychee],
-    Color: [red],
+    name: "lychee",
+    Color: "red",
     Seed: [1],
-    origin: [china, malaysia],
+    origin: "china, malaysia",
     tasty: true,
   },
   {
-    name: [mangosteen],
-    Color: [purple],
+    name:"mangosteen",
+    Color: "purple",
     Seed: [1, 2],
-    origin: [malaysia, indoneisa],
+    origin: "malaysia, indoneisa",
     tasty: true,
   },
   {
-    name: [passionfruit],
-    Color: [purple],
+    name: "passionfruit",
+    Color: "purple",
     Seed: [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70],
-    origin: [mexico],
+    origin: "mexico, brazil",
     tasty: true,
   },
   {
-    name: [starfruit],
-    Color: [yellow],
+    name: "starfruit",
+    Color: "yellow",
     Seed: [5, 6, 7, 8, 9, 10],
-    origin: [indonesia, mexico],
+    origin: "indonesia, mexico",
     tasty: true,
   },
   {
-    name: [longan],
-    Color: [brown],
+    name: "longan",
+    Color: "brown",
     Seed: [1],
-    origin: [china, indonesia],
+    origin: "china, indonesia",
     tasty: true,
   },
   {
-    name: [persimmon],
-    Color: [orange],
+    name: "persimmon",
+    Color: "orange",
     Seed: [5, 6, 7, 8],
-    origin: [china, japan, korea],
+    origin: "china, japan, korea",
     tasty: false,
   },
   {
-    name: [pomegranate],
-    Color: [red],
+    name: "pomegranate",
+    Color: "red",
     Seed: [550, 560, 570, 580, 590, 600],
-    origin: [india, iran],
+    origin: "india, iran",
     tasty: true,
   },
   {
-    name: [kiwi],
-    Color: [brown],
+    name: "kiwi",
+    Color: "brown",
     Seed: [450, 460, 470, 480, 490, 500],
-    origin: [china],
+    origin: "china, newzealand",
     tasty: true,
   },
   {
-    name: [kumquat],
-    Color: [yellow],
+    name: "kumquat",
+    Color: "yellow",
     Seed: [1, 2, 3, 4],
-    origin: [china],
+    origin: "china,japan",
     tasty: true,
   },
   {
-    name: [jackfruit],
-    Color: [yellow],
+    name: "jackfruit",
+    Color: "yellow",
     Seed: [100],
-    origin: [india, malaysia],
+    origin: "india, malaysia",
     tasty: false,
   },
   {
-    name: [fig],
-    Color: [purple],
+    name: "fig",
+    Color: "purple",
     Seed: [600, 610, 620, 630, 640, 650],
-    origin: [india],
+    origin: "india , malaysia",
     tasty: false,
   },
 ];
-fruits.forEach((fruit) => console.log(fruit.name)); //console logs names of fruit
 
-const outcome = fruits.filter((fruit) => fruit.seed < 100); //don't need to name, atuo-returns your contents,dont need to use return
-console.log(outcome);
+fruit.forEach((fruit) => {
+  console.log(fruit.name); //console logs names of fruit
+fruit.forEach((Seed)) => {
+  console.log(fruit.Seed);
+
+const result = fruit.filter((fruit) =>{return fruit.Seed < 100; //don't need to name, atuo-returns your contents,dont need to use return
+console.log(result)});
